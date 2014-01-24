175f8ad2d6be97ee4fa87b766188f3244194b2e1
gtchen66 <gtchen66@yahoo.com>
2014-01-24 09:23:02 -0800

    Initial Commit

---
+++ TestingAudio/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestingAudio
+//
+//  Created by George Chen on 1/24/14.
+//  Copyright (c) 2014 George Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TestingAudio/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestingAudio
+//
+//  Created by George Chen on 1/24/14.
+//  Copyright (c) 2014 George Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
