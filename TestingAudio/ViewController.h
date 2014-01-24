9290a315677d5c4869c0fc78882c93823b68466c
gtchen66 <gtchen66@yahoo.com>
2014-01-24 10:23:02 -0800

    Simple App to record and playback
    Based on tutorial at:
    
    http://www.techotopia.com/index.php/Recording_Audio_on_an_iPhone_with_AV
    AudioRecorder_%28iOS_4%29

--- TestingAudio/ViewController.h
+++ TestingAudio/ViewController.h
@@ -7,7 +7,22 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <AVAudioRecorderDelegate, AVAudioPlayerDelegate> {
+    AVAudioRecorder *audioRecorder;
+    AVAudioPlayer *audioPlayer;
+    UIButton *playButton;
+    UIButton *recordButton;
+    UIButton *stopButton;
+}
+
+@property (nonatomic, retain) IBOutlet UIButton *playButton;
+@property (nonatomic, retain) IBOutlet UIButton *recordButton;
+@property (nonatomic, retain) IBOutlet UIButton *stopButton;
+
+-(IBAction) recordAudio;
+-(IBAction) playAudio;
+-(IBAction) stop;
 
 @end
